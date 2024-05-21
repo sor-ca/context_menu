52687ad1020b84031c88939d9b325d2505f5e850
sor-ca <mariagrandova@gmail.com>
2024-05-21 18:09:12 +0300

    second variant

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 use egui::{
     menu::{BarState, MenuRoot},
-    Color32, Id, InnerResponse, Pos2, Response, Sense, Ui,
+    Color32, Id, InnerResponse, Pos2, Rect, Response, Sense, Ui,
 };
+
+const RED_RECT: Rect = Rect {
+    min: Pos2::new(10., 10.),
+    max: Pos2::new(100., 100.),
+};
+
+const BLUE_RECT: Rect = Rect {
+    min: Pos2::new(100., 10.),
+    max: Pos2::new(200., 100.),
+};
+
+const BIG_RECT: Rect = Rect {
+    min: Pos2::new(10., 10.),
+    max: Pos2::new(200., 100.),
+};
+
+#[derive(Default, Debug, serde::Deserialize, serde::Serialize)]
+enum Color {
+    #[default]
+    Red,
+    Blue,
+}
 
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
@@ -20,19 +42,13 @@ fn main() -> eframe::Result<()> {
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // This how you opt-out of serialization of a field
-    menu_pos: Option<Pos2>,
+    state: Color,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "white".to_owned(),
-            menu_pos: None,
+            state: Color::default(),
         }
     }
 }
@@ -56,70 +72,85 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let mut action = false;
             let size = ui.available_size_before_wrap();
             let (_, painter) = ui.allocate_painter(size, Sense::click());
-            let big_rect = egui::Rect {
-                min: Pos2::new(10., 10.),
-                max: Pos2::new(200., 200.),
-            };
-            painter.rect_filled(big_rect.clone(), 0., Color32::LIGHT_GRAY);
-            let r = ui.allocate_rect(big_rect.clone(), Sense::click());
 
-            let small_rect1 = egui::Rect {
-                min: Pos2::new(20., 40.),
-                max: Pos2::new(40., 60.),
-            };
+            let r = ui.allocate_rect(BIG_RECT, Sense::click());
 
-            painter.rect_filled(small_rect1.clone(), 0., Color32::RED);
-            //let r1 = ui.allocate_rect(small_rect1.clone(), Sense::click());
+            painter.rect_filled(RED_RECT, 0., Color32::RED);
 
-            let small_rect2 = egui::Rect {
-                min: Pos2::new(50., 40.),
-                max: Pos2::new(70., 60.),
-            };
+            painter.rect_filled(BLUE_RECT, 0., Color32::BLUE);
 
-            painter.rect_filled(small_rect2.clone(), 0., Color32::BLUE);
-
-            // if r.secondary_clicked() {
-            //     println!("r clicked2");
-            //     let ctx = ui.ctx();
-            //     if let Some(pos) = ctx.input(|i| i.pointer.hover_pos()) {
-            //         if small_rect1.contains(pos) {
-            //             self.label = "red".to_owned();
-            //         } else if small_rect2.contains(pos) {
-            //             self.label = "blue".to_owned();
-            //         } else {
-            //             self.label = "gray".to_owned();
-            //         }
-            //     }
-            // }
-
-            context_menu_custom(&r, self, |ui| {
-                if ui.button("change color").clicked() {
-                    action = true;
-                }
-            });
-            if action {
-                if let Some(pos) = self.menu_pos {
-                    if small_rect1.contains(pos) {
-                        self.label = "red".to_owned();
-                    } else if small_rect2.contains(pos) {
-                        self.label = "blue".to_owned();
-                    } else {
-                        self.label = "gray".to_owned();
-                    }
-                }
-            }
-            ui.label(self.label.clone());
+            context_menu_custom(&r, &mut self.state, set_state, set_contents);
         });
     }
 }
 
-pub fn context_menu_custom(
+fn set_state(p: Pos2) -> Color {
+    if RED_RECT.contains(p) {
+        Color::Red
+    } else if BLUE_RECT.contains(p) {
+        Color::Blue
+    } else {
+        Color::default()
+    }
+}
+
+fn set_contents(state: &Color) -> Box<dyn FnOnce(&mut Ui)> {
+    match state {
+        Color::Red => Box::new(|ui| {
+            if ui.button("red").clicked() {
+                dbg!("red");
+            }
+        }),
+        Color::Blue => Box::new(|ui| {
+            if ui.button("blue").clicked() {
+                dbg!("blue");
+            }
+        }),
+    }
+}
+
+pub fn context_menu_custom_example(
     response: &Response,
-    app: &mut TemplateApp,
-    add_contents: impl FnOnce(&mut Ui),
+    state: &mut Color,
+) -> Option<InnerResponse<()>> {
+    let menu_id = Id::new("__egui::context_menu");
+    let mut bar_state = BarState::load(&response.ctx, menu_id);
+    let root = &mut bar_state;
+
+    let menu_response = MenuRoot::context_interaction(response, root);
+    match &menu_response {
+        egui::menu::MenuResponse::Create(p, _) => *state = set_state(*p),
+
+        _ => {}
+    };
+
+    let add_contents = match state {
+        Color::Red => |ui: &mut Ui| {
+            if ui.button("red").clicked() {
+                dbg!("red");
+            }
+        },
+        Color::Blue => |ui: &mut Ui| {
+            if ui.button("blue").clicked() {
+                dbg!("blue");
+            }
+        },
+    };
+
+    MenuRoot::handle_menu_response(root, menu_response);
+    let inner_response = bar_state.show(response, add_contents);
+
+    bar_state.store(&response.ctx, menu_id);
+    inner_response
+}
+
+pub fn context_menu_custom<T>(
+    response: &Response,
+    state: &mut T,
+    get_state: impl FnOnce(Pos2) -> T,
+    set_contents: impl Fn(&T) -> Box<dyn FnOnce(&mut Ui)>,
 ) -> Option<InnerResponse<()>> {
     let menu_id = Id::new("__egui::context_menu");
     let mut bar_state = BarState::load(&response.ctx, menu_id);
@@ -128,14 +159,13 @@ pub fn context_menu_custom(
     let menu_response = MenuRoot::context_interaction(response, root);
     match &menu_response {
         egui::menu::MenuResponse::Create(p, _) => {
-            app.menu_pos = Some(*p);
-        }
-        egui::menu::MenuResponse::Close => {
-            app.menu_pos = None;
-            app.label = "white".to_owned();
+            *state = get_state(*p);
         }
         _ => {}
     };
+
+    let add_contents = set_contents(&state);
+
     MenuRoot::handle_menu_response(root, menu_response);
     let inner_response = bar_state.show(response, add_contents);
 
